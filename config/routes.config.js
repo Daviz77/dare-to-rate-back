e76812fad9dcb6cbc5e7f55810c7c84b0c9e7847
Daviz77 <daviddlcalle @gmail.com>
2023-04-03 20:56:37 +0200

    feat: AuthController & login route

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const healthController = require('../controllers/health.controller')
-const usersController = require('../controllers/users.controller')
+const healthController = require("../controllers/health.controller")
+const usersController = require("../controllers/users.controller")
+const authController = require("../controllers/auth.controller")
 
-router.get('/health', healthController.health)
+router.get("/health", healthController.health)
+
+// AUTHENTICATION
+
+router.post("/login", authController.login)
 
 // USER ROUTES
 
-router.post('/users', usersController.create);
-router.get('/users', usersController.list);
+router.post("/users", usersController.create)
+router.get("/users", usersController.list)
 
 // REVIEW ROUTES
 
 // COMMENT ROUTES
-
-
 
 module.exports = router
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,20 +1,22 @@
-const User = require('../models/User.model')
-const Review = require('../models/Review.model')
-const Comment = require('../models/Comment.model')
-const { StatusCodes } = require('http-status-codes');
+const User = require("../models/User.model")
+const Review = require("../models/Review.model")
+const Comment = require("../models/Comment.model")
+const { StatusCodes } = require("http-status-codes")
+const createError = require("http-errors")
 
-// { "email": "pablo@email.com", "password": "12345678" }
+
 module.exports.create = (req, res, next) => {
-  const { email, password, username, img, about } = req.body;
-  User.create({ email, password, username, img, about })
-    .then(userCreated => {
-      res.status(StatusCodes.CREATED).json(userCreated);
-    })
-    .catch(next)
+	const { email, password, username, img, about } = req.body
+	User.create({ email, password, username, img, about })
+		.then((userCreated) => {
+			res.status(StatusCodes.CREATED).json(userCreated)
+		})
+		.catch(next)
 }
 
 module.exports.list = (req, res, next) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(next)
+	User.find()
+		.then((users) => res.json(users))
+		.catch(next)
 }
+
