60f42912997f2878427bbba249905ba730fd4131
Daviz77 <daviddlcalle @gmail.com>
2023-04-03 21:02:39 +0200

    refactor: Auth middelware

--- middlewares/Auth.middlewares.js
+++ middlewares/Auth.middlewares.js
@@ -1,15 +1,44 @@
-module.exports.isAuthenticated = (req, res, next) => {
-  if (req.user) {
-    next()
-  } else {
-    res.redirect('/login')
-  }
-}
+const jwt = require("jsonwebtoken")
+const createError = require("http-errors")
+const { StatusCodes } = require("http-status-codes")
 
-module.exports.isNotAuthenticated = (req, res, next) => {
-  if (!req.user) {
-    next()
-  } else {
-    res.redirect('/profile')
-  }
+module.exports.isAuthenticated = (req, res, next) => {
+	const authorization = req.header("Authorization")
+
+	if (!authorization) {
+		return next(
+			createError(
+				StatusCodes.UNAUTHORIZED,
+				"Authorization header was not provided"
+			)
+		)
+	}
+
+	const [schema, token] = authorization.split(" ")
+
+	if (schema !== "Bearer") {
+		return next(
+			createError(
+				StatusCodes.UNAUTHORIZED,
+				"Authorization schema is not supported"
+			)
+		)
+	}
+
+	if (!token) {
+		return next(
+			createError(StatusCodes.UNAUTHORIZED, "A token must be provided")
+		)
+	}
+
+	// a comprobarlo con JWT
+	const secret = process.env.JWT_SECRET || "test"
+	jwt.verify(token, secret, (err, decodedToken) => {
+		if (err) {
+			return next(err)
+		}
+
+		req.currentUser = decodedToken.id
+		next()
+	})
 }
