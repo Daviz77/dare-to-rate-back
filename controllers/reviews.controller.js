7dc18103f82e391c7db0cf0c261f980e7267a468
Daviz77 <daviddlcalle @gmail.com>
2023-04-16 16:03:43 +0200

    fix:  delete review

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -13,25 +13,23 @@ module.exports.create = (req, res, next) => {
 	}
 
 	if (!film._id) {
-		return Film.findOne({ imdbId: film.imdbId })
-			.then(filmRes => {
-				if (!filmRes) {
-					return Film.create(film)
-						.then(filmCreated => createReview({ ...review, author, film: filmCreated._id }, res, next))
-						.catch(next)
-				}
+		return Film.findOne({ imdbId: film.imdbId }).then((filmRes) => {
+			if (!filmRes) {
+				return Film.create(film)
+					.then((filmCreated) => createReview({ ...review, author, film: filmCreated._id }, res, next))
+					.catch(next)
+			}
 
-				createReviewIfUserDoesNotYetHaveOne({ ...review, author, film: filmRes._id }, res, next)
-			})
+			createReviewIfUserDoesNotYetHaveOne({ ...review, author, film: filmRes._id }, res, next)
+		})
 	}
-
 
 	createReviewIfUserDoesNotYetHaveOne({ ...review, author, film: film._id }, res, next)
 }
 
 const createReviewIfUserDoesNotYetHaveOne = (review, res, next) =>
 	Review.find({ author: review.author })
-		.then(reviews => {
+		.then((reviews) => {
 			if (reviews.length > 0) return next(createError(StatusCodes.CONFLICT, 'User already has a review'))
 			createReview(review, res, next)
 		})
@@ -47,18 +45,20 @@ module.exports.getAllReviews = (req, res, next) => {
 
 	if (req.currentUserId) {
 		return User.findById(req.currentUserId)
-		.then(user =>
-			Review.find({ author: { $in: user.followings } })
-				.limit(reviewsLimit)
-				.sort({ createdAt: -1 })
-				.populate('author', 'username img')
-				.then(followedReviews => 
-					Review.find({ author: { $ne: user.followings }})
-						.limit(reviewsLimit)
-						.sort({ createdAt: -1 })
-						.populate('author', 'username img')
-					.then(otherReviews => res.json({ data: { followedReviews , otherReviews }})))
-				.catch(next))
+			.then((user) =>
+				Review.find({ author: { $in: user.followings } })
+					.limit(reviewsLimit)
+					.sort({ createdAt: -1 })
+					.populate('author', 'username img')
+					.then((followedReviews) =>
+						Review.find({ author: { $ne: user.followings } })
+							.limit(reviewsLimit)
+							.sort({ createdAt: -1 })
+							.populate('author', 'username img')
+							.then((otherReviews) => res.json({ data: { followedReviews, otherReviews } }))
+					)
+					.catch(next)
+			)
 			.catch(next)
 	}
 
@@ -66,7 +66,7 @@ module.exports.getAllReviews = (req, res, next) => {
 		.limit(reviewsLimit)
 		.sort({ createdAt: -1 })
 		.populate('author', 'username img')
-		.then((reviews) => res.json({ data: { reviews }}))
+		.then((reviews) => res.json({ data: { reviews } }))
 		.catch(next)
 }
 
@@ -84,23 +84,27 @@ module.exports.updateReview = (req, res, next) => {
 	Review.findById(reviewId)
 		.then((review) => {
 			if (review.author._id + '' === req.currentUserId) {
-				return Review.findByIdAndUpdate(
-					id,
-					{ title, content, likes },
-					{ new: true, runValidators: true }
-				)
+				return Review.findByIdAndUpdate(id, { title, content, likes }, { new: true, runValidators: true })
 					.then((review) => res.status(StatusCodes.OK).json({ data: review }))
 					.catch(next)
 			}
 
-			return next(createError( StatusCodes.UNAUTHORIZED, 'You are not the author of this review'))
+			return next(createError(StatusCodes.UNAUTHORIZED, 'You are not the author of this review'))
 		})
 		.catch(next)
 }
 
 module.exports.deleteReview = (req, res, next) => {
 	const { reviewId } = req.params
-	Review.findByIdAndDelete(reviewId)
-		.then(() => res.status(StatusCodes.OK))
+
+	Review.findById(reviewId)
+		.then((review) => {
+			if (!review) return next(createError(StatusCodes.NOT_FOUND, 'Review not found'))
+			if (review.author._id.toString() !== req.currentUserId)
+				return next(createError(StatusCodes.UNAUTHORIZED, 'User are not the author of this review'))
+
+			review.deleteOne()
+			return res.status(StatusCodes.OK).send()
+		})
 		.catch(next)
 }
